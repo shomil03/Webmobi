6b8e7f59579886d5ff4b51b04a876f964f3cd17f
shomil03 <shomil02020@gmail.com>
2024-06-07 21:27:31 +0530

    Initial Commit

---
+++ Webmobi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Webmobi
+//
+//  Created by Shomil Singh on 07/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Webmobi/WebmobiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebmobiApp.swift
+//  Webmobi
+//
+//  Created by Shomil Singh on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WebmobiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
